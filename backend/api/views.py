3b4f0d066764cd32eb6dbbbdf7dbe73a40fe83b9
Ilya Fabiyanskiy <ilya.fabiyanskiy@yandex.ru>
2023-09-24 23:56:38 +0300

    1

--- backend/api/views.py
+++ backend/api/views.py
@@ -196,7 +196,7 @@ class RecipesViewSet(viewsets.ModelViewSet):
         ).select_related('author').prefetch_related(
             'tags', 'ingredients', 'recipe',
             'shopping_cart', 'favorite_recipe'
-        ) if self.request.user.is_authenticated else Recipe.objects.annotate(
+        ) if self.request.user else Recipe.objects.annotate(
             is_in_shopping_cart=Value(False),
             is_favorited=Value(False),
         ).select_related('author').prefetch_related(
--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -109,7 +109,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
