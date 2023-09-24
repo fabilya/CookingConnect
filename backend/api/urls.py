b41763faca76124cbcf4ba68a4b6005b00f53bc8
Ilya Fabiyanskiy <ilya.fabiyanskiy@yandex.ru>
2023-09-25 01:16:24 +0300

    g

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -14,24 +14,19 @@ router.register('recipes', RecipesViewSet)
 
 
 urlpatterns = [
-     path(
-          'auth/token/login/',
+     path('auth/token/login/',
           AuthToken.as_view(),
           name='login'),
-     path(
-          'users/set_password/',
+     path('users/set_password/',
           set_password,
           name='set_password'),
-     path(
-          'users/<int:user_id>/subscribe/',
+     path('users/<int:user_id>/subscribe/',
           AddAndDeleteSubscribe.as_view(),
           name='subscribe'),
-     path(
-          'recipes/<int:recipe_id>/favorite/',
+     path('recipes/<int:recipe_id>/favorite/',
           AddDeleteFavoriteRecipe.as_view(),
           name='favorite_recipe'),
-     path(
-          'recipes/<int:recipe_id>/shopping_cart/',
+     path('recipes/<int:recipe_id>/shopping_cart/',
           AddDeleteShoppingCart.as_view(),
           name='shopping_cart'),
      path('', include(router.urls)),
