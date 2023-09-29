524b9374fd23aff59e0f4d174d113fec0d998387
Ilya Fabiyanskiy <ilya.fabiyanskiy@yandex.ru>
2023-09-29 09:54:08 +0300

    -

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.db.models import ForeignKey, Model, CASCADE, DateTimeField, \
-    UniqueConstraint, CheckConstraint
+from django.db.models import (ForeignKey, Model, CASCADE, DateTimeField,
+                              UniqueConstraint, CheckConstraint)
 
 
 class User(AbstractUser):
