323c688f45d4e35493723c0f4239c6b53852b13e
Ilya Fabiyanskiy <ilya.fabiyanskiy@yandex.ru>
2023-10-01 23:25:25 +0300

    11

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -5,10 +5,7 @@ from rest_framework.permissions import SAFE_METHODS
 class OwnerUserOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        return (
-            request.method in SAFE_METHODS
-            or request.user.is_authenticated
-            and request.user.is_active
-            and request.user == obj.author
-            or request.user.is_staff
-        )
+        if request.method in SAFE_METHODS:
+            return True
+
+        return obj.user == request.user
