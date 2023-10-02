5ca9d5125e353bb747060c1e7400856e4b2ee79b
Ilya Fabiyanskiy <ilya.fabiyanskiy@yandex.ru>
2023-10-02 12:47:10 +0300

    13

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -120,7 +120,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'api.pagination.PageLimitPagination',
     'PAGE_SIZE': 6,
 }
 
